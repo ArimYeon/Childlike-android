5433a335e005da0b396b07842cc4c83b84f9353e
yeonarim <sdr04161@gmail.com>
2020-06-12 15:09:24 +0900

    Mypage에 children 정보 api에서 리스트로 가져와 보여주기

--- app/src/main/java/com/example/childlike/MypageActivity.java
+++ app/src/main/java/com/example/childlike/MypageActivity.java
@@ -7,15 +7,25 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.childlike.dataclass.MypageItem;
+import com.example.childlike.retrofit.RetrofitManager;
+import com.example.childlike.retrofit.retrofitdata.RequestChildrenGet;
+import com.example.childlike.retrofit.retrofitdata.RequestChildrenPost;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import static com.example.childlike.LoginActivity.CODE;
+import static com.example.childlike.LoginActivity.kuid;
 
 public class MypageActivity extends AppCompatActivity {
 
@@ -23,9 +33,12 @@ public class MypageActivity extends AppCompatActivity {
     private final static int RESULT_FROM_MYPAGE_CODE = 201;
     public static String SELECTED_USER;
 
-    ArrayList<MypageItem> list = new ArrayList<>();
+    ArrayList<RequestChildrenGet> list = new ArrayList<>();
+    //ArrayList<RequestChildrenGet> retrofitList = new ArrayList<>;
     ImageView backBtn;
     Button addUserBtn, memberInfoBtn;
+
+    MypageAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +49,7 @@ public class MypageActivity extends AppCompatActivity {
         addUserBtn = findViewById(R.id.add_user_btn);
         memberInfoBtn = findViewById(R.id.member_info_btn);
         setListeners();
-        initList();
+        retrofitChildrenGet();
 
         SharedPreferences a = getSharedPreferences("a", MODE_PRIVATE);
         SharedPreferences.Editor edit = a.edit();
@@ -46,11 +59,11 @@ public class MypageActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         // 리사이클러뷰에 MypageAdapter 객체 지정.
-        MypageAdapter adapter = new MypageAdapter(list);
+        adapter = new MypageAdapter(list);
         adapter.setOnButtonClickListener(new MypageAdapter.OnButtonClickListener() {
             @Override
             public void onButtonClick(View v, int position) {
-                MypageItem item = list.get(position);
+                RequestChildrenGet item = list.get(position);
                 String name = item.getName();
                 Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
                 intent.putExtra("name", name);
@@ -61,7 +74,7 @@ public class MypageActivity extends AppCompatActivity {
         adapter.setOnRadioButtonClickListener(new MypageAdapter.OnRadioButtonClickListener() {
             @Override
             public void onRadioButtonClick(int position) {
-                MypageItem item = list.get(position);
+                RequestChildrenGet item = list.get(position);
                 String name = item.getName();
                 SELECTED_USER = name;
                 SharedPreferences a = getSharedPreferences("a", MODE_PRIVATE);
@@ -98,12 +111,37 @@ public class MypageActivity extends AppCompatActivity {
         });
     }
 
-    private void initList(){
-        list.add(new MypageItem(R.drawable.img_110x110, "연아림", "24","여자"));
-        list.add(new MypageItem(R.drawable.img_110x110, "나희원", "24","여자"));
-        list.add(new MypageItem(R.drawable.img_110x110, "이슬기", "24","여자"));
-        list.add(new MypageItem(R.drawable.img_110x110, "정주연", "24","여자"));
-        list.add(new MypageItem(R.drawable.img_110x110, "이호진", "23","여자"));
-        list.add(new MypageItem(R.drawable.img_110x110, "김연정", "23","여자"));
+    private void retrofitChildrenGet(){
+        Call<ArrayList<RequestChildrenGet>> call = RetrofitManager.createApi().getChildren(kuid);
+        call.enqueue(new Callback<ArrayList<RequestChildrenGet>> (){
+            @Override
+            public void onResponse(Call<ArrayList<RequestChildrenGet>> call, Response<ArrayList<RequestChildrenGet>> response) {
+                if (response.isSuccessful()) {
+                    list = response.body();
+                    //Log.d("mypageList", list.get(0).getName()+list.get(0).getId());
+                    //setMypageDataList(retrofitList);
+                    adapter.setData(list);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<RequestChildrenGet>> call, Throwable t) {
+                Log.d("children_get","데이터 get 실패");
+            }
+        });
     }
+
+    /*
+    private void setMypageDataList(ArrayList<RequestChildrenGet> retrofitList){
+        for(int i=0; i<retrofitList.size(); i++){
+            list.add(new MypageItem(
+                    retrofitList.get(i).getImage(),
+                    retrofitList.get(i).getName(),
+                    retrofitList.get(i).getAge(),
+                    retrofitList.get(i).getGender()
+            ));
+        }
+        //adapter.setData();
+    }
+    */
 }
--- app/src/main/java/com/example/childlike/retrofit/IRetrofitApi.java
+++ app/src/main/java/com/example/childlike/retrofit/IRetrofitApi.java
@@ -1,8 +1,12 @@
 package com.example.childlike.retrofit;
 
 import com.example.childlike.retrofit.retrofitdata.RequestAppuserPost;
+import com.example.childlike.retrofit.retrofitdata.RequestChildrenGet;
 import com.example.childlike.retrofit.retrofitdata.RequestChildrenPost;
 
+
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
@@ -10,6 +14,7 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
+import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
@@ -42,5 +47,10 @@ public interface IRetrofitApi {
     @POST("api/children/")
     Call<RequestChildrenPost> postChildren(
             @Body RequestChildrenPost children
+    );
+
+    @GET("api/children/{uid}")
+    Call<ArrayList<RequestChildrenGet>> getChildren(
+            @Path("uid") String userId
     );
 }
